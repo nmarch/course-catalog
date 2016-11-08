ab3a25fa15c238b29712d72d18823af9759aa3af
nickymarch1@hotmail.com <nmarch@vcd-ch204-14.eastern.ewu.edu>
2016-11-08 14:41:57 -0800

    Version 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
 // YOUR SCRIPTS
+
+
+// GRID VIEW
+
+$('#grid').click(function(){
+    
+    // Remove ONLY 'list' Class
+    $('.cd-gallery').removeClass('list');
+    
+    //Add ONLY 'grid' Class
+    $('.cd-gallery').addClass('grid');
+   
+    
+});
+
+// List View
+
+$('#list').click(function(){
+    
+    // Remove ONLY 'list' Class
+    $('.cd-gallery').removeClass('grid');
+    
+    //Add ONLY 'grid' Class
+    $('.cd-gallery').addClass('list');
+   
+    
+});
