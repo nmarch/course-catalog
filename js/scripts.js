ee557b55be1d53f940638daebcc6ca2e7d999f37
Nicky March <nickymarch1@hotmail.com>
2016-12-09 16:20:24 -0800

    Version 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
 // YOUR SCRIPTS
+
+
+//Grid View
+$('#grid').click( function(){
+    
+    
+    //Remove ONLY 'list' Class
+    $('.cd-gallery').removeClass('list');
+    //Add ONLY 'grid' Class
+    $('.cd-gallery').addClass('grid');
+    
+    console.log('Vote');
+    
+    
+    
+} );
+
+//List View
+$('#list').click( function(){
+    
+    
+    //Remove ONLY 'grid' Class
+    $('.cd-gallery').removeClass('grid');
+    //Add ONLY 'list' Class
+    $('.cd-gallery').addClass('list');
+    
+    console.log('Vote');
+    
+    
+    
+} );
